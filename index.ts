9d9631970701a4bfd9feb2a07e955c26ffc0fb90
Harald Kirschner <hkirschner@microsoft.com>
2023-03-21 14:40:14 -0700

    first commit

---
+++ index.ts
@@ -0,0 +1,61 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+interface User {
+  id: number;
+  name: string;
+  job: string;
+  city: string;
+  email: string;
+}
+
+const app = express();
+const port = 3001;
+let users: User[] = [];
+
+app.use(bodyParser.json());
+
+app.get('/users', (req, res) => {
+  const timezone = req.get('Timezone');
+  res.send(users);
+});
+
+app.get('/users/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const user = users.find(user => user.id === id);
+  if (user) {
+    res.send(user);
+  } else {
+    res.status(404).send(`User with ID ${id} not found`);
+  }
+});
+
+app.post('/users', (req, res) => {
+  const user = req.body;
+  users.push(user);
+  res.send('User added successfully');
+});
+
+app.put('/users/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const updateUser = req.body;
+  const index = users.findIndex(user => user.id === id);
+  if (index !== -1) {
+    users[index] = { ...users[index], ...updateUser };
+    res.send('User updated successfully');
+  } else {
+    res.status(404).send(`User with ID ${id} not found`);
+  }
+});
+
+app.delete('/users/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  users = users.filter(user => user.id !== id);
+  res.send('User deleted successfully');
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
+
+export default app;
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Corp Addressbook
+
+Making it easier to find people in the company.
+
+## TODO
+
+- [ ] Create a package.json and other necessary configurations to run the index.ts file.
+- [ ] Load in-memory user data from JSON file.
+- [ ] Move `port` configuration to .env file.
+- [ ] Secures the addressbook with username/password (use `passport` library).
+- [ ] Maybe: Add countries, country code and timezone for each user.
